ca48deece0600e2e29a3150c2c13a49d9f8b6e9e
Japanese FrameNet <jfn@192-168-1-153.pvt670.hc.keio.ac.jp>
2015-12-16 20:03:35 +0900

    changed frame definition display.

--- src/main/webapp/resources/js/test.js
+++ src/main/webapp/resources/js/test.js
@@ -2,6 +2,19 @@
  * Created by jfn on 12/8/15.
  */
 
-function showReport(){
+function showMes() {
+    alert("Hello World!");
+}
 
+function toggle_login(id) {
+    var div = document.getElementById(id);
+    if (div.style.height == 'auto') {
+        div.style.height = '40px';
+    } else {
+        div.style.height = 'auto';
+    }
+    //var content = $(div).html();
+    //var c = content.substr(0, content.length - 5);
+    //var html = c + " ...";
+    //$(this).html(html);
 }
