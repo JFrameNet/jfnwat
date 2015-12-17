a1c425253cac2818b5b56ce1413247479d91c2e8
Japanese FrameNet <jfn@192-168-1-153.pvt670.hc.keio.ac.jp>
2015-12-17 12:02:49 +0900

    Review style for search result page.

--- src/main/webapp/resources/js/test.js
+++ src/main/webapp/resources/js/test.js
@@ -8,13 +8,13 @@ function showMes() {
 
 function toggle_login(id) {
     var div = document.getElementById(id);
+    var content = $(div).html();
     if (div.style.height == 'auto') {
         div.style.height = '40px';
+        var c = content.substr(0, content.length - 5);
+        var html = c + " ...";
+        $(div).html(html);
     } else {
         div.style.height = 'auto';
     }
-    //var content = $(div).html();
-    //var c = content.substr(0, content.length - 5);
-    //var html = c + " ...";
-    //$(this).html(html);
 }
