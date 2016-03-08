913ae6eeb140d85b7865efc5ddb5a9bfbb7486de
Quiri Passchier <Quiri@hotmail.com>
2016-03-08 16:03:13 +0900

    disabled login for testing purposes

--- src/main/java/jp/keio/jfn/wat/SecurityConfig.java
+++ src/main/java/jp/keio/jfn/wat/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
 
             http.authorizeRequests()
-                .antMatchers("/resources/**").permitAll()
+                .antMatchers("/**").permitAll() // resources/
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
