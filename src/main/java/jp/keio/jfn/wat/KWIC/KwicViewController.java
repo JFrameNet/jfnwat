33ca469aeeddba950e0f9949978f5dffbaab526f
Quiri Passchier <Quiri@hotmail.com>
2016-03-10 15:38:52 +0900

    fixed kwic index button and  set display to non for general use before first push

--- src/main/java/jp/keio/jfn/wat/KWIC/KwicViewController.java
+++ src/main/java/jp/keio/jfn/wat/KWIC/KwicViewController.java
@@ -1,8 +1,7 @@
 package jp.keio.jfn.wat.KWIC;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import javax.faces.bean.ManagedBean;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Quiri on 3-2-2016.
@@ -13,6 +12,5 @@ import javax.faces.bean.ManagedBean;
 public class KwicViewController {
 
     public String toKwic() { return "kwicPage?faces-redirect=true";}
-
 }
 
