63f51dcd0102faa031e49b6eb6f90712c12051fa
Japanese FrameNet <jfn@192-168-1-153.pvt670.hc.keio.ac.jp>
2016-04-18 10:52:18 +0900

    missing from previous commit

--- src/main/java/jp/keio/jfn/wat/repository/LexemeRepository.java
+++ src/main/java/jp/keio/jfn/wat/repository/LexemeRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.repository.CrudRepository;
  */
 public interface LexemeRepository extends CrudRepository<Lexeme, Long> {
     Lexeme findById(int id);
+    Lexeme findByName(String name);
 }
